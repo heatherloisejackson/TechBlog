ea39bd5f78740a5c695b2a073667b507ffcbec02
Heather Jackson <hmjackson917@gmail.com>
2021-06-02 20:51:46 -0400

    further fixes of the word project to entry

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -3,12 +3,12 @@ const { Entry } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newEntry = await Entry.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newEntry);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -16,19 +16,19 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const entryData = await Entry.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!entryData) {
+      res.status(404).json({ message: 'No entry found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(entryData);
   } catch (err) {
     res.status(500).json(err);
   }
