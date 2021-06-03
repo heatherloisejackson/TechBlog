1ee81c1ada74867854361d64c045818324a7468c
Heather Jackson <hmjackson917@gmail.com>
2021-06-02 20:43:02 -0400

    updated project keyword to entry

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const entryRoutes = require('./entryRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/entries', entryRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Entry = require('./Entry');
 
-User.hasMany(Project, {
+User.hasMany(Entry, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Entry.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Entry };
